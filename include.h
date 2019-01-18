567ad7d445b66257a34d0b05be0acc02149b3d38
SUNGBUMPARK <psbum1609@gmail.com>
2019-01-18 20:04:01 +0900

    Common include

---
+++ include.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <conio.h>
+
+#define FILE_NAME_SIZE 101
+#define VERY_BIG_SIZE 0xFFFFFFF
+#define ASCII_SIZE 128
